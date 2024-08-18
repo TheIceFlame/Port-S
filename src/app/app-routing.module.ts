417cd77708c08840d102fc4eb32abdf3fbdd33bc
Youssef Ben Abdessalem <77058937+Sasuk1@users.noreply.github.com>
2024-08-19 00:30:21 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
 
 const routes: Routes = [];
+routes.push({ path: '', redirectTo: 'home', pathMatch: 'full' });
+
+routes.push({ path: 'home', component: HomeComponent });
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
